a0db12d8b4d3e67a3c62968a4cea563c7d7eb223
Evan M Jones <evanjones4040@gmail.com>
2018-10-25 21:04:32 -0700

    better images in list

--- src/components/media.js
+++ src/components/media.js
@@ -2,6 +2,16 @@ import { h } from "wigly-jsx";
 import Raw from "./raw";
 import "./media.css";
 
+var abs = {
+  objectFit: "contain",
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  background: "grey"
+};
+
 export default class Media {
   mounted(el) {
     if (!this.props.media_embed.content) return;
@@ -14,9 +24,37 @@ export default class Media {
     frame.style.left = "0";
   }
 
+  isImage(str) {
+    var last = str.split(".").pop();
+    var rules = { jpg: true, jpeg: true, png: true, gif: true };
+    return rules[last];
+  }
+
+  formatImageURL(str) {
+    if (str.indexOf("imgur.com") === -1) {
+      return str;
+    }
+
+    var parts = str.split(".");
+    var last = parts.pop();
+
+    return parts.join(".") + "m." + last;
+  }
+
   render() {
-    var { media_embed } = this.props;
-    if (!media_embed.content) return;
+    var { media_embed, url } = this.props;
+
+    if (!media_embed.content && this.isImage(url)) {
+      return (
+        <div style={{ position: "relative", paddingTop: `${(9 / 16) * 100}%`, marginTop: "10px" }}>
+          <img style={abs} src={this.formatImageURL(url)} />
+        </div>
+      );
+    }
+
+    if (!media_embed.content) {
+      return;
+    }
 
     var photo = { "imgur.com": true };
 
@@ -24,7 +62,7 @@ export default class Media {
       <div style={{ position: "relative", paddingTop: `${(9 / 16) * 100}%`, marginTop: "10px" }}>
         {photo[this.props.media.type] || this.props.image ? (
           <a onclick={e => e.stopPropagation()} href={this.props.href || this.props.media.oembed.thumbnail_url}>
-            <img style={{ objectFit: "cover" }} src={this.props.media.oembed.thumbnail_url} />
+            <img style={abs} src={this.props.media.oembed.thumbnail_url} />
           </a>
         ) : (
           <Raw content={media_embed.content} />
